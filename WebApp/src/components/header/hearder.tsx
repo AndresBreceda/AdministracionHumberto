4e064ac8b54f452b165682ff317ce96f2a7e0829
Andrés Breceda <a.e.breceda@gmail.com>
2025-07-13 17:30:27 -0600

    Comienzo del proyecto primera pantalla

---
+++ WebApp/src/components/header/hearder.tsx
@@ -0,0 +1,16 @@
+import { FileSpreadsheet, TableOfContents } from "lucide-react";
+
+export default function Header() {
+  return (
+    <header className="bg-[var(--gris1)] absolute w-full px-4 py-2 flex justify-between items-center">
+      {/* Grupo izquierdo: ícono + texto */}
+      <div className="flex items-center gap-2">
+        <FileSpreadsheet className="w-6 h-6 text-white" />
+        <p className="font-bold text-white">Pronto CV</p>
+      </div>
+
+      {/* Ícono derecho */}
+      <TableOfContents className="w-6 h-6 text-white" />
+    </header>
+  );
+}
--- WebApp/src/components/home/home.tsx
+++ WebApp/src/components/home/home.tsx
@@ -1,9 +1,27 @@
+import Header from "../header/hearder";
 
-export default function Home(){
+export default function Home() {
+  return (
+    <>
+      <Header />
 
-    return(
-        <div>
-            Hola mundo
+      <div className="flex h-screen">
+        {/* Panel izquierdo */}
+        <div className="flex-1 bg-[var(--azul2)] flex flex-col justify-center px-12">
+          <h1 className="text-white text-3xl font-extrabold mb-6">
+            Ten tu currículum vitae en segundos
+          </h1>
+          <p className="text-white text-2xl font-bold leading-relaxed">
+            Crea tu CV con plantillas personalizadas <br />
+            y listas para usarse. Escoge, <br />
+            llena tu información en el formulario <br />
+            ¡y listo!
+          </p>
         </div>
-    );
+
+        {/* Panel derecho */}
+        <div className="flex-1 bg-[var(--gris2)]"></div>
+      </div>
+    </>
+  );
 }
