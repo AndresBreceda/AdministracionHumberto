5be24314f50e0cac94acecb13f1c17c8709521a6
Andrés Breceda <a.e.breceda@gmail.com>
2025-07-13 20:19:05 -0600

    primera pantalla animacion

--- WebApp/src/components/header/hearder.tsx
+++ WebApp/src/components/header/hearder.tsx
@@ -2,7 +2,7 @@ import { FileSpreadsheet, TableOfContents } from "lucide-react";
 
 export default function Header() {
   return (
-    <header className="bg-[var(--gris1)] absolute w-full px-4 py-2 flex justify-between items-center">
+    <header className="bg-[var(--gris1)] absolute w-full px-4 py-2 flex justify-between items-center z-50">
       {/* Grupo izquierdo: ícono + texto */}
       <div className="flex items-center gap-2">
         <FileSpreadsheet className="w-6 h-6 text-white" />
--- WebApp/src/components/home/home.tsx
+++ WebApp/src/components/home/home.tsx
@@ -1,11 +1,38 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 import Header from "../header/hearder";
 
 export default function Home() {
+  const imagesRef = useRef<(HTMLImageElement | null)[]>([]);
+
+
+  useEffect(() => {
+    let current = 0;
+    const total = imagesRef.current.length;
+
+    const timeline = gsap.timeline({ repeat: -1, repeatDelay: 1 });
+
+    for (let i = 0; i < total; i++) {
+      timeline
+        .to(imagesRef.current[i], {
+          opacity: 1,
+          duration: 1,
+          ease: "power2.inOut",
+        })
+        .to(imagesRef.current[i], {
+          opacity: 0,
+          duration: 1,
+          ease: "power2.inOut",
+          delay: 2, // tiempo visible antes de desaparecer
+        });
+    }
+  }, []);
+
   return (
     <>
       <Header />
 
-      <div className="flex h-screen">
+      <div className="flex min-h-screen">
         {/* Panel izquierdo */}
         <div className="flex-1 bg-[var(--azul2)] flex flex-col justify-center px-12">
           <h1 className="text-white text-3xl font-extrabold mb-6">
@@ -20,7 +47,26 @@ export default function Home() {
         </div>
 
         {/* Panel derecho */}
-        <div className="flex-1 bg-[var(--gris2)]"></div>
+        <div className="flex-2 flex-col bg-[var(--gris2)] flex items-center justify-center relative">
+          <img src="chicos.svg" className="w-[500px] z-10" />
+
+          {/* CVs superpuestos */}
+          <div className="absolute top-20 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[220px] h-auto z-0">
+            {["cv1.png", "cv2.png", "cv3.png"].map((src, i) => (
+            <img
+              key={i}
+              ref={(el) => {
+                imagesRef.current[i] = el;
+              }}
+              src={src}
+              className="absolute top-0 left-0 w-full opacity-0"
+            />
+          ))}
+
+          </div>
+          <h2 className="mt-20 text-2xl font-bold text-black">Crea el tuyo ¡Ahora!</h2>
+          <button className="bg-[var(--azul3)] mt-5 text-xl px-8 py-4 rounded-full hover:bg-blue-700 transition">Comenzar</button>
+        </div>
       </div>
     </>
   );
